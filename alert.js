a82481333b96c97ca7ca055a7e0221c5d9d5d2b5
MezbaulHossain <mdmhnishat@gmail.com>
2024-11-08 12:46:46 +0600

    all

---
+++ alert.js
@@ -0,0 +1,7 @@
+// alert( '3 +
+//     1
+//     +2' );
+// "use strict";
+// alert("Hello");
+
+// [1, 2].forEach(alert);
---
+++ type.js
@@ -0,0 +1,11 @@
+// let name = "Ilya";
+// let name;
+// alert( `hello ${1}` ); // ?
+
+// alert( `hello ${"name"}` ); // ?
+
+// alert( `hello ${name}` ); // ?
+let a = prompt("First number?");
+let b = prompt("Second number?");
+parseInt(line[0])
+alert(Number(a) +Number(b)); // 12
